4a7fcb7f18168fe623a6c7c885e015a9160f1cf2
AlexIstra1989 <alex888fcsm@gmail.com>
2021-09-05 02:55:58 +0300

    fix bugs

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -2,8 +2,9 @@ function test(a, b) {
     return a + b
 }
 
-console.log(test(5, 2));
-console.log(test(2, 4));
-console.log(test(9, 1));
+$(function(){
+  var mixer = mixitup('.portfolio__content');
+  
+});
 
 //# sourceMappingURL=script.js.map
