f7c57c2b4890baec2a9ee21c69d7202bde0989ad
AlexIstra1989 <alex888fcsm@gmail.com>
2021-09-10 23:59:11 +0300

    fix

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,7 +1,3 @@
-function test(a, b) {
-    return a + b
-}
-
 $(function(){
   var mixer = mixitup('.portfolio__content');
   
--- gulpfile.js
+++ gulpfile.js
@@ -53,9 +53,9 @@ const path = {
 
 // Если нужно выполнять преобразование файлов в определенном порядке, то используем массив с нужным нам порядком:
 const jsFiles = [
-    srcPath + 'assets/js/lib.js',
     srcPath + 'assets/js/main.js'
 ]
+
 
 
 // TASKS
@@ -172,6 +172,7 @@ function js(cb) {
 
     cb();
 }
+
 
 // Для быстрой компиляции JS во время разработки 
 function jsWatch(cb) {
