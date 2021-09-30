01acf774bd8db47dfe19aaba7460aef6aa0dc38f
AlexIstra1989 <alex888fcsm@gmail.com>
2021-09-30 23:12:15 +0300

    fix

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,7 +1,15 @@
+const { tree } = require("gulp");
+
 $(function(){
   var mixer = mixitup('.portfolio__content');
   
-  $('.slider-blog__inner').slick();
+  $('.slider-blog__inner').slick({
+    dots: tree
+  });
+
+  $('.menu__burger').on('click', function(){
+    $('.header__top-inner').toggleClass('.header__top-inner--active');
+  });
 
 });
 
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,14 @@
+const { tree } = require("gulp");
+
 $(function(){
   var mixer = mixitup('.portfolio__content');
   
-  $('.slider-blog__inner').slick();
+  $('.slider-blog__inner').slick({
+    dots: tree
+  });
+
+  $('.menu__burger').on('click', function(){
+    $('.header__top-inner').toggleClass('.header__top-inner--active');
+  });
 
 });
